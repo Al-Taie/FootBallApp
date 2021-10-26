af5b6447d6e6ea361fdc95f46f9223ffd4af6a1b
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-10-26 19:55:20 +0300

    feat: design match fragment ui

---
+++ app/src/main/java/com/watermelon/footballapp/ui/match/MatchFragment.kt
@@ -0,0 +1,22 @@
+package com.watermelon.footballapp.ui.match
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.watermelon.footballapp.ui.base.BaseFragment
+import com.watermelon.footballapp.viewModels.MatchViewModel
+import watermelon.footballapp.BR
+import watermelon.footballapp.databinding.FragmentMatchBinding
+
+
+class MatchFragment : BaseFragment<FragmentMatchBinding>() {
+    override fun setup() {
+
+    }
+
+    override val viewModelID = BR.matchViewModel
+    override val viewModel: MatchViewModel by activityViewModels()
+    override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentMatchBinding
+        get() = FragmentMatchBinding::inflate
+
+}
