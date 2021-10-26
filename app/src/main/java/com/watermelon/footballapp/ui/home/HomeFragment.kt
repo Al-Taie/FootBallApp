d1d239157419a09a42488e30bf2e0376899e8ccf
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-10-26 18:50:03 +0300

    feat: design home ui

--- app/src/main/java/com/watermelon/footballapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/watermelon/footballapp/ui/home/HomeFragment.kt
@@ -6,15 +6,15 @@ import androidx.fragment.app.activityViewModels
 import com.watermelon.footballapp.ui.base.BaseFragment
 import com.watermelon.footballapp.viewModels.HomeViewModel
 import watermelon.footballapp.BR
-import watermelon.footballapp.databinding.HomeFragmentBinding
+import watermelon.footballapp.databinding.FragmentHomeBinding
 
 
-class HomeFragment : BaseFragment<HomeFragmentBinding>() {
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override fun setup() {}
 
     override val viewModelID = BR.viewModel
     override val viewModel: HomeViewModel by activityViewModels()
-    override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> HomeFragmentBinding
-        get() = HomeFragmentBinding::inflate
+    override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentHomeBinding
+        get() = FragmentHomeBinding::inflate
 
 }
