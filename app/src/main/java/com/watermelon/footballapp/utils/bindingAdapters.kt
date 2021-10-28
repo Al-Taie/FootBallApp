299e34a0622b9476827010cb51a4b0d74052edd0
Al-Taie <agprosup@gmail.com>
2021-10-28 17:07:41 +0300

    feat: create set recycler binding adapter

--- app/src/main/java/com/watermelon/footballapp/utils/bindingAdapters.kt
+++ app/src/main/java/com/watermelon/footballapp/utils/bindingAdapters.kt
@@ -1,2 +1,10 @@
 package com.watermelon.footballapp.utils
 
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.watermelon.footballapp.ui.base.BaseAdapter
+
+@BindingAdapter(value = ["app:items"])
+fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
+    (view.adapter as BaseAdapter<T>?)?.setItems(items ?: emptyList())
+}
