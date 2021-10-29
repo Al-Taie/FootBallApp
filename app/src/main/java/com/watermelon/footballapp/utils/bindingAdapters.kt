86c922d66e9b0c9c640d8c67210c46bda5592a2d
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-10-29 04:09:55 +0300

    feat: add binding adapter for match status

--- app/src/main/java/com/watermelon/footballapp/utils/bindingAdapters.kt
+++ app/src/main/java/com/watermelon/footballapp/utils/bindingAdapters.kt
@@ -1,12 +1,23 @@
 package com.watermelon.footballapp.utils
 
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.watermelon.footballapp.model.response.match.Match
 import com.watermelon.footballapp.ui.base.BaseAdapter
 
 @BindingAdapter(value = ["app:items"])
 fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
     (view.adapter as BaseAdapter<T>?)?.setItems(items ?: emptyList())
     Log.v("MAIN_ACTIVITY", items.toString())
+}
+
+@BindingAdapter(value = ["matchType"])
+fun setMatchType(view: TextView, match: Match?) {
+    when(match?.status) {
+        "SCHEDULED" -> view.text = match.utcDate
+        "FINISHED" -> view.text = "${match.score?.fullTime?.homeTeam} - ${match?.score?.fullTime?.awayTeam}"
+    }
 }
