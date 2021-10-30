e0b7a24f9d61e9b99dd491952ed13c505ee65487
Wesam Nabeel <wesamezio@gmail.com>
2021-10-31 02:27:21 +0300

    feature : make team request

--- app/src/main/java/com/watermelon/footballapp/ui/team/TeamViewModel.kt
+++ app/src/main/java/com/watermelon/footballapp/ui/team/TeamViewModel.kt
@@ -1,8 +1,12 @@
 package com.watermelon.footballapp.ui.team
 
 import androidx.lifecycle.ViewModel
-import com.watermelon.footballapp.ui.match.MatchInteractionListener
+import androidx.lifecycle.asLiveData
+import com.watermelon.footballapp.model.repository.FootBallRepository
 
 class TeamViewModel : ViewModel() {
+    private val repository = FootBallRepository()
+
+    val team = repository.getTeamById(20).asLiveData()
 
 }
