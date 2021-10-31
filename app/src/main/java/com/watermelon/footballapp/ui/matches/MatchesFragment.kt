05ccebb29d0b547d1db2e4f356425b8a3094ca67
Wesam Nabeel <wesamezio@gmail.com>
2021-10-31 03:06:24 +0300

    feature : apply matches to match navigation with safe args

--- app/src/main/java/com/watermelon/footballapp/ui/matches/MatchesFragment.kt
+++ app/src/main/java/com/watermelon/footballapp/ui/matches/MatchesFragment.kt
@@ -3,14 +3,27 @@ package com.watermelon.footballapp.ui.matches
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
+import com.watermelon.footballapp.ui.adapter.MatchAdapter
 import com.watermelon.footballapp.ui.base.BaseFragment
+import com.watermelon.footballapp.ui.home.HomeFragmentDirections
 import watermelon.footballapp.databinding.FragmentMatchesBinding
 
-class MatchesFragment: BaseFragment<FragmentMatchesBinding>() {
-    override fun setup() {}
+class MatchesFragment: BaseFragment<FragmentMatchesBinding>(),MatchesNavigator {
+    override fun setup() {
+        binding.matchesRecycler.adapter = MatchAdapter(emptyList(), viewModel)
+        viewModel.navigator = this
+    }
 
     override val viewModel: MatchesViewModel by activityViewModels()
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentMatchesBinding
         get() = FragmentMatchesBinding::inflate
+
+    override fun navigateToMatch(id: Int) {
+        val action = MatchesFragmentDirections.actionMatchesFragmentToMatchFragment(id)
+        Navigation.findNavController(binding.root)
+            .navigate(action)
+
+    }
 
 }
--- app/src/main/java/com/watermelon/footballapp/ui/matches/MatchesViewModel.kt
+++ app/src/main/java/com/watermelon/footballapp/ui/matches/MatchesViewModel.kt
@@ -1,10 +1,16 @@
 package com.watermelon.footballapp.ui.matches
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.watermelon.footballapp.model.repository.FootBallRepository
 import com.watermelon.footballapp.ui.match.MatchInteractionListener
 
 class MatchesViewModel : ViewModel() ,MatchInteractionListener {
+    private val repository = FootBallRepository()
+    lateinit var navigator: MatchesNavigator
+    val matches = repository.getMatches().asLiveData()
+
     override fun onMatchClicked(id: Int) {
-        TODO("Not yet implemented")
+        navigator.navigateToMatch(id)
     }
 }
