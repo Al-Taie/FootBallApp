999f5d885ef9000dbf6c502f757b7fae8e267004
Al-Taie <agprosup@gmail.com>
2021-11-01 21:49:05 +0300

    feat: implement event observer

--- app/src/main/java/com/watermelon/footballapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/watermelon/footballapp/ui/home/HomeFragment.kt
@@ -1,35 +1,34 @@
 package com.watermelon.footballapp.ui.home
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.watermelon.footballapp.ui.adapter.MatchAdapter
 import com.watermelon.footballapp.ui.base.BaseFragment
+import com.watermelon.footballapp.utils.Event
 import watermelon.footballapp.databinding.FragmentHomeBinding
 
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(), HomeNavigator {
-
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val viewModel: HomeViewModel by activityViewModels()
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentHomeBinding
         get() = FragmentHomeBinding::inflate
 
     override fun setup() {
         binding.matchesRecycler.adapter = MatchAdapter(emptyList(), viewModel)
-        viewModel.navigator = this
-        binding.listener = viewModel
+        viewModel.navigateToTeam.observe(this, ::navigateToTeam)
     }
 
-    override fun navigateToMatch(id: Int) {
-        val action = HomeFragmentDirections.actionHomeFragmentToMatchFragment(id)
-        findNavController().navigate(action)
-    }
+//    fun navigateToMatch(id: Int) {
+//        val action = HomeFragmentDirections.actionHomeFragmentToMatchFragment(id)
+//        findNavController().navigate(action)
+//    }
 
-    override fun navigateToTeam(id: Int) {
-        val action = HomeFragmentDirections.actionHomeFragmentToTeamFragment(id)
-        findNavController().navigate(action)
+    private fun navigateToTeam(event: Event<Int>) {
+        event.getContentIfNotHandled()?.let { id ->
+            val action = HomeFragmentDirections.actionHomeFragmentToTeamFragment(id)
+            findNavController().navigate(action)
+        }
     }
-
 }
