1d2f885a5faa3f0c5bf877bfcd1740aaa07052c3
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-11-06 14:32:05 +0300

    create competition match adapter

--- app/src/main/java/com/watermelon/footballapp/ui/matches/MatchesFragment.kt
+++ app/src/main/java/com/watermelon/footballapp/ui/matches/MatchesFragment.kt
@@ -4,18 +4,19 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
+import com.watermelon.footballapp.ui.adapter.CompetitionMatchAdapter
 import com.watermelon.footballapp.ui.adapter.MatchAdapter
 import com.watermelon.footballapp.ui.base.BaseFragment
+import com.watermelon.footballapp.ui.competition.CompetitionViewModel
 import com.watermelon.footballapp.ui.home.HomeFragmentDirections
 import watermelon.footballapp.databinding.FragmentMatchesBinding
 
 class MatchesFragment: BaseFragment<FragmentMatchesBinding>(),MatchesNavigator {
     override fun setup() {
-        binding.matchesRecycler.adapter = MatchAdapter(emptyList(), viewModel)
-        viewModel.navigator = this
+        binding.matchesRecycler.adapter = CompetitionMatchAdapter(emptyList(), viewModel)
     }
 
-    override val viewModel: MatchesViewModel by activityViewModels()
+    override val viewModel: CompetitionViewModel by activityViewModels()
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentMatchesBinding
         get() = FragmentMatchesBinding::inflate
 
