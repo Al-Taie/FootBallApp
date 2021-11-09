904f4db308824b5359835d492d1d3b3a1e3eaa46
Al-Taie <agprosup@gmail.com>
2021-11-09 21:37:53 +0300

    fix: use bundle in MatchesFragment

--- app/src/main/java/com/watermelon/footballapp/ui/matches/MatchesFragment.kt
+++ app/src/main/java/com/watermelon/footballapp/ui/matches/MatchesFragment.kt
@@ -3,23 +3,20 @@ package com.watermelon.footballapp.ui.matches
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.Navigation
 import com.watermelon.footballapp.ui.adapter.CompetitionMatchAdapter
-import com.watermelon.footballapp.ui.adapter.MatchAdapter
 import com.watermelon.footballapp.ui.base.BaseFragment
-import com.watermelon.footballapp.ui.competition.CompetitionViewModel
-import com.watermelon.footballapp.ui.home.HomeFragmentDirections
+import com.watermelon.footballapp.utils.Constants
 import watermelon.footballapp.databinding.FragmentMatchesBinding
 
-class MatchesFragment: BaseFragment<FragmentMatchesBinding>(),MatchesNavigator {
+class MatchesFragment : BaseFragment<FragmentMatchesBinding>(), MatchesNavigator {
     override fun setup() {
+        arguments?.getInt(Constants.ID)?.run { viewModel.getCompetitionMatchesById(this) }
         binding.matchesRecycler.adapter = CompetitionMatchAdapter(emptyList(), viewModel)
     }
 
-    override val viewModel: CompetitionViewModel by activityViewModels()
+    override val viewModel: MatchesViewModel by activityViewModels()
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentMatchesBinding
         get() = FragmentMatchesBinding::inflate
 
     override fun navigateToMatch(id: Int) {}
-
 }
