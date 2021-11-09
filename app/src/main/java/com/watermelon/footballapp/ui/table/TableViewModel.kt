787f4eadd6e5d3b801a916d97f02ee0c2649c23b
Al-Taie <agprosup@gmail.com>
2021-11-09 21:37:21 +0300

    fix: use bundle in TableFragment

--- app/src/main/java/com/watermelon/footballapp/ui/table/TableViewModel.kt
+++ app/src/main/java/com/watermelon/footballapp/ui/table/TableViewModel.kt
@@ -1,13 +1,30 @@
 package com.watermelon.footballapp.ui.table
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.*
+import com.watermelon.footballapp.model.State
 import com.watermelon.footballapp.model.repository.FootBallRepository
+import com.watermelon.footballapp.model.response.standings.StandingsResponse
+import com.watermelon.footballapp.ui.match.MatchInteractionListener
 import com.watermelon.footballapp.ui.team.TeamInteractionListener
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class TableViewModel : ViewModel(),TeamInteractionListener {
+class TableViewModel : ViewModel(), MatchInteractionListener {
+    private val _competitionStanding = MutableLiveData<State<StandingsResponse?>>()
+    val competitionStanding : LiveData<State<StandingsResponse?>>
+        get() = _competitionStanding
 
-    override fun onTeamClicked(id: Int) {
+    fun getCompetitionStandingById(competitionId: Int) {
+        viewModelScope.launch {
+            FootBallRepository.getCompetitionStandingsById(competitionId).collect {
+                _competitionStanding.postValue(it)
+            }
+        }
     }
+
+    override fun onMatchClicked(id: Int) {
+        // TODO("Not yet implemented")
+    }
+
 
 }
