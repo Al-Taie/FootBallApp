808285fb14af322829894f5fac1b9a00d16f9581
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-11-10 02:01:21 +0300

    fix: player request

--- app/src/main/java/com/watermelon/footballapp/ui/player/PlayerFragment.kt
+++ app/src/main/java/com/watermelon/footballapp/ui/player/PlayerFragment.kt
@@ -3,11 +3,15 @@ package com.watermelon.footballapp.ui.player
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
 import com.watermelon.footballapp.ui.base.BaseFragment
 import watermelon.footballapp.databinding.FragmentPlayerBinding
 
 class PlayerFragment: BaseFragment<FragmentPlayerBinding>() {
-    override fun setup() {}
+    override fun setup() {
+        val args : PlayerFragmentArgs by navArgs()
+        viewModel.getPlayerById(args.id)
+    }
 
     override val viewModel: PlayerViewModel by activityViewModels()
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentPlayerBinding
--- app/src/main/java/com/watermelon/footballapp/ui/player/PlayerViewModel.kt
+++ app/src/main/java/com/watermelon/footballapp/ui/player/PlayerViewModel.kt
@@ -1,10 +1,24 @@
 package com.watermelon.footballapp.ui.player
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.*
+import com.watermelon.footballapp.model.State
 import com.watermelon.footballapp.model.repository.FootBallRepository
+import com.watermelon.footballapp.model.response.player.PlayerRespopnse
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class PlayerViewModel : ViewModel() {
-    val player = FootBallRepository.getPlayerById(20).asLiveData()
+
+    private val _player = MutableLiveData<State<PlayerRespopnse?>>()
+    val player : LiveData<State<PlayerRespopnse?>>
+        get() = _player
+
+    fun getPlayerById(playerId : Int){
+        viewModelScope.launch {
+            FootBallRepository.getPlayerById(playerId).collect {
+                _player.postValue(it)
+            }
+        }
+    }
 
 }
