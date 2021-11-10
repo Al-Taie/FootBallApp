f231ba15aa010969deb96cbc11c2ac673bcb1b3b
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-11-10 08:50:08 +0300

    feat: use custom bottom navigation bar

--- app/src/main/java/com/watermelon/footballapp/ui/MainActivity.kt
+++ app/src/main/java/com/watermelon/footballapp/ui/MainActivity.kt
@@ -2,8 +2,8 @@ package com.watermelon.footballapp.ui
 
 import androidx.activity.viewModels
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.watermelon.footballapp.ui.base.BaseActivity
+import com.watermelon.footballapp.utils.onNavDestinationSelected
 import com.watermelon.footballapp.viewModels.MainViewModel
 import watermelon.footballapp.R
 import watermelon.footballapp.databinding.ActivityMainBinding
@@ -20,7 +20,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun onResume() {
         super.onResume()
         val navController = findNavController(R.id.fragment_container_host)
-        binding.bottomNavigation.setupWithNavController(navController)
+
+        binding.bottomNavigation.addBubbleListener { id ->
+            onNavDestinationSelected(id , navController)
+        }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigation.setSelectedWithId(destination.id, false)
+        }
     }
 
 }
--- app/src/main/java/com/watermelon/footballapp/utils/extensions.kt
+++ app/src/main/java/com/watermelon/footballapp/utils/extensions.kt
@@ -1,7 +1,11 @@
 package com.watermelon.footballapp.utils
 
 import android.view.View
+import androidx.navigation.ActivityNavigator
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import com.watermelon.footballapp.model.State
+import watermelon.footballapp.R
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -31,4 +35,35 @@ fun <T> View.handleSuccessState(state: State<T>?) = if (state is State.Success) 
     this.visibility = View.VISIBLE
 } else {
     this.visibility = View.GONE
+}
+
+fun onNavDestinationSelected(
+    itemId: Int,
+    navController: NavController
+): Boolean {
+    val builder = NavOptions.Builder()
+        .setLaunchSingleTop(true)
+    if (navController.currentDestination!!.parent!!.findNode(itemId) is ActivityNavigator.Destination) {
+        builder.setEnterAnim(R.anim.nav_default_enter_anim)
+            .setExitAnim(R.anim.nav_default_exit_anim)
+            .setPopEnterAnim(R.anim.nav_default_pop_enter_anim)
+            .setPopExitAnim(R.anim.nav_default_pop_exit_anim)
+    } else {
+        builder.setEnterAnim(R.animator.nav_default_enter_anim)
+            .setExitAnim(R.animator.nav_default_exit_anim)
+            .setPopEnterAnim(R.animator.nav_default_pop_enter_anim)
+            .setPopExitAnim(R.animator.nav_default_pop_exit_anim)
+    }
+    //if (itemId == getChildAt(0).id) {
+    //builder.setPopUpTo(findStartDestination(navController.graph)!!.id, true)
+    // }
+    builder.setPopUpTo(itemId, true)
+    val options = builder.build()
+    return try {
+
+        navController.navigate(itemId, null, options)
+        true
+    } catch (e: IllegalArgumentException) {
+        false
+    }
 }
